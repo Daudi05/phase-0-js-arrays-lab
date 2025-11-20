9516bd843de6e67dceec732db018d38a35f12d8d
dwege <ojorodavid@gmail.com>
2025-11-20 15:39:47 +0300

    initial commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,28 @@
 // Write your code here
 
+const products = ["laptop", "phone", "headphones", "Monitor"]
+function logFirstProduct() {
+  console.log(products[0])
+} 
+
+// logFirstProduct()
+
+function addProduct(productName) {
+  products[4] = productName
+  
+}
+addProduct("fridge")
+console.log(products) //
+function updateProductName(position, productName) {
+  products[position] = productName
+}
+updateProductName(1, "samsung")
+console.log(products)
+function removeLastProduct() {
+  products.pop()
+}
+removeLastProduct()
+console.log(products)
 
 
 // Export the necessary parts for testing
